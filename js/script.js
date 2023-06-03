7a3c183e5eb6425437c4b50d496fa3abcb2a5a36
xOms5917 <106932369+xOms5917@users.noreply.github.com>
2023-06-03 19:14:25 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ noButton.addEventListener("mouseover", () => {
 const yesButton = document.getElementById("yes-button");
 
 // Add a click event listener to create confetti
-yesButton.addEventListener("click", () => {
+yesButton.addEventListener("click", () => { alert(p)})
   
 var confettiElement = document.getElementById('confetti-canvas');
 var confettiSettings = { target: confettiElement, max: 729, size: 1, animate: true, props: ['circle', 'square', 'triangle', 'line'], colors: [[165,104,246],[230,61,135],[0,199,228],[253,214,126]], clock: 25, rotate: true,start_from_edge: true, respawn: true };
